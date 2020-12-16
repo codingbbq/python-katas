8f48933e1d772e4794c86b90c7e6e0a0e21fd120
codingbbq <codingbbq@gmail.com>
2020-12-16 19:26:13 +0530

    Advent of Code 2015 - Day 06 Python solution

--- advent_of_code/2015/day06.py
+++ advent_of_code/2015/day06.py
@@ -1,37 +1,94 @@
 class Solution2015:
+    MAX = 1000
 
+    # Function to process the statements and extract data to be used later.
     def processStmt(self, instruction):
+        
         ilist = instruction.split(" ")
+        processedData = {}
         if ilist[0] == "turn" and ilist[1] == "on":
-            return self.switch("on", ilist[2], ilist[4])
+            x = ilist[2].split(',')
+            y = ilist[4].split(',')
+            processedData["action"] = "on"
+            processedData["cX"] = [x[0], x[1]]
+            processedData["cY"] = [y[0], y[1]]
+            return processedData
 
         if ilist[0] == "turn" and ilist[1] == "off":
-            return self.switch("off", ilist[2], ilist[4])
-
+            x = ilist[2].split(',')
+            y = ilist[4].split(',')
+            processedData["action"] = "off"
+            processedData["cX"] = [x[0], x[1]]
+            processedData["cY"] = [y[0], y[1]]
+            return processedData
+            
         if ilist[0] == "toggle":
-            return self.switch("toggle", ilist[1], ilist[3])
-
-    # perform actions on the lights
-    def switch(self, action, cX, cY):
-        i = cX.split(',')
-        xi, yi = i[0], i[1]
-
-        j = cY.split(',')
-        xj, yj = j[0], j[1]
-
-        return xi
-
+            x = ilist[1].split(',')
+            y = ilist[3].split(',')
+            processedData["action"] = "toggle"
+            processedData["cX"] = [x[0], x[1]]
+            processedData["cY"] = [y[0], y[1]]
+            return processedData
 
     def litLights(self, inpt):
+        # Let us turn off all the lights
+        lightMatrix = []
+        lightMatrix = [[0 for i in range(self.MAX)] for j in range(self.MAX)]
         count = 0
         for stmt in inpt:
-            return self.processStmt(stmt)
-        
+            data = {}
+            data = self.processStmt(stmt)
+            for i in range(int(data['cX'][0]), 1 + int(data['cY'][0])):
+                    for j in range(int(data['cX'][1]), 1 + int(data['cY'][1])):
+                        if data["action"] == "on":
+                            if lightMatrix[i][j] == 0:
+                                lightMatrix[i][j] = 1
+                                count+=1
+                        if data["action"] == "off":
+                            if lightMatrix[i][j] == 1:
+                                lightMatrix[i][j] = 0
+                                count-=1
+                        if data["action"] == "toggle":
+                            if lightMatrix[i][j] == 1:
+                                lightMatrix[i][j] = 0
+                                count-=1
+                            else:
+                                lightMatrix[i][j] = 1
+                                count+=1
+
+        return count
+
+    def brightness(self, inpt):
+        # Let us turn off all the lights
+        lightMatrix = []
+        lightMatrix = [[0 for i in range(self.MAX)] for j in range(self.MAX)]
+        count = 0
+        for stmt in inpt:
+            data = {}
+            data = self.processStmt(stmt)
+            for i in range(int(data['cX'][0]), 1 + int(data['cY'][0])):
+                    for j in range(int(data['cX'][1]), 1 + int(data['cY'][1])):
+                        if data["action"] == "on":
+                            lightMatrix[i][j]+= 1
+                            count+=1
+
+                        if data["action"] == "off":
+                            if lightMatrix[i][j] > 0:
+                                lightMatrix[i][j] -= 1
+                                count-=1
+                                
+                        if data["action"] == "toggle":
+                            lightMatrix[i][j] += 2
+                            count+=2
+
+        return count
 
 files = open("./advent_of_code/2015/input/day06.txt", "r")
 inpt = [line.rstrip() for line in files.readlines()]
-
-lightMatrix = []
-
-puzzle1 = Solution2015().litLights(["turn on 0,0 through 999,999"])
+puzzle1 = Solution2015().litLights(inpt)
 print(puzzle1)
+
+puzzle2 = Solution2015().brightness(inpt)
+print(puzzle2)
+
+files.close()

e03f4e6ab9f5e4654659afeadcb886f96fa82061
codingbbq <codingbbq@gmail.com>
2020-12-18 15:45:10 +0530

    Added comments

--- advent_of_code/2015/day07.py
+++ advent_of_code/2015/day07.py
@@ -1,7 +1,10 @@
 class Solution2015:
     def processInstructions(self, instructions, mapOfKnownInputs):
         for indice, i in enumerate(instructions):
+            # Flag to use in the list when the instuction is successfully executed
             remove = False
+
+            # Split the string to get input and output
             ii = str(i).split("->")
             input = ii[0].strip()
             output = ii[1].strip()
@@ -10,10 +13,12 @@ class Solution2015:
             if "b" in mapOfKnownInputs:
                 mapOfKnownInputs['b'] = 956
             
+            #If the input is just a number
             if input.isdigit():
                 mapOfKnownInputs[output] = input
-                remove = True
+                remove = True # Flag to Remove this instruction from the list
             else:
+                # Break the input based on various conditions
                 x = self.inputbreakup(input)
                 if x["wire1"] in mapOfKnownInputs and x["wire2"] in mapOfKnownInputs:
 
@@ -63,15 +68,19 @@ class Solution2015:
                         mapOfKnownInputs[output] = int(mapOfKnownInputs[x["wire1"]]) >> int(x["wire2"])
                         remove = True
 
+            # To remove the instuction from the list, we assign 0 as the value
             if remove == True:
                 instructions[indice] = 0
 
+        # Create a new list from the existing list by discarding 0
         updatedInstructions = []
         updatedInstructions = [t for t in instructions if t != 0]
         
+        # Perform this operation until the list has no instructions
         if(len(updatedInstructions) > 0):
             self.processInstructions(updatedInstructions, mapOfKnownInputs)
 
+        # This dictionary contains all the wires and the value that it has
         return mapOfKnownInputs
 
     def inputbreakup(self, input):
